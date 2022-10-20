316340376754e624db0c680ba8281606399217d7
luong <luongck21v7k019@vlvh.ctu.edu.vn>
2022-10-20 09:13:43 +0700

    Tao trang cap nhat lien he

--- src/router/Index.js
+++ src/router/Index.js
@@ -4,8 +4,7 @@ const routes = [
   {
     path: "/",
     name: "contactbook",
-    component: ContactBook,
-  },
+    component: ContactBook, },
   {
     path: "/:pathMatch(.*)*",
     name: "notfound",
@@ -15,8 +14,14 @@ const routes = [
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
-    props: true // Truyền các biến trong $route.params vào làm props
-    },
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/contacts",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
